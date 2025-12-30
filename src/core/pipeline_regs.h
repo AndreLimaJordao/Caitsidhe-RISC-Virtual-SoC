16ea672fd804597678e1cab10bdfda90db7fae93
AndréLJ <96208310+AndreLimaJordao@users.noreply.github.com>
2025-12-30 18:36:45 -0300

    Add pipeline register structures for IF-ID, ID-EX, EX-MEM, and MEM-WB stages

--- src/core/pipeline_regs.h
+++ src/core/pipeline_regs.h
@@ -2,7 +2,57 @@
 // Created by andre on 29/12/2025.
 //
 
-#ifndef PIPELINE_REGS_H
-#define PIPELINE_REGS_H
+#pragma once
+#include "../common/types.h"
 
-#endif //PIPELINE_REGS_H
+namespace cvsim {
+
+    struct IF_ID_Regs {
+        addr_t pc;
+        inst_t instruction;
+
+        bool valid;
+    };
+
+    struct ID_EX_Reg {
+        addr_t pc;
+        inst_t instruction;
+        word_t rs1_val;
+        word_t rs2_val;
+        word_t imm;
+        reg_addr_t rd;
+        Opcode opcode;
+        InstrType instr_type;
+
+        bool WB[2];      // [0] = RegWrite, [1] = MemtoReg
+        bool M[3];       // [0] = MemRead, [1] = MemWrite, [2] = Branch
+        bool EX[3];      // [0] = ALUSrc, [1] = ALUOp1, [2] = ALUOp0
+
+        bool valid;
+    };
+
+    struct EX_MEM_Reg {
+        addr_t pc;
+        inst_t instruction;
+        word_t alu_result;
+        word_t rs2_val;
+        reg_addr_t rd;
+
+        bool WB[2];             // [0] = RegWrite, [1] = MemtoReg
+        bool M[3];              // [0] = MemRead, [1] = MemWrite, [2] = Branch
+
+        bool valid;
+    };
+
+    struct MEM_WB_Reg {
+        addr_t pc;
+        inst_t instruction;
+        word_t mem_data;
+        word_t alu_result;
+        reg_addr_t rd;
+
+        bool WB[2];             // [0] = RegWrite, [1] = MemtoReg
+
+        bool valid;
+    };
+}
