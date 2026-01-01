6cc1d8a57a3496c78f53de233a60686029941f5a
AndréLJ <96208310+AndreLimaJordao@users.noreply.github.com>
2026-01-01 12:50:26 -0300

    Improve fetch/decode execution flow

--- src/core/cpu.h
+++ src/core/cpu.h
@@ -4,6 +4,7 @@
 
 #pragma once
 #include "../common/types.h"
+#include "../common/utils.h"
 #include "../memory/bus.h"
 #include "regfile.h"
 #include "alu.h"
@@ -18,28 +19,27 @@ class CPU {
         Bus& bus;
         cvsim::word_t pc;
 
-        cvsim::IF_ID_Reg IF_ID_Curr;
-        cvsim::IF_ID_Reg IF_ID_Next;
-        cvsim::ID_EX_Reg ID_EX_Curr;
-        cvsim::ID_EX_Reg ID_EX_Next;
-        cvsim::EX_MEM_Reg EX_MEM_Curr;
-        cvsim::EX_MEM_Reg EX_MEM_Next;
-        cvsim::MEM_WB_Reg MEM_WB_Curr;
-        cvsim::MEM_WB_Reg MEM_WB_Next;
+        cvsim::IF_ID_Reg IF_ID_Curr, IF_ID_Next;
+        cvsim::ID_EX_Reg ID_EX_Curr, ID_EX_Next;
+        cvsim::EX_MEM_Reg EX_MEM_Curr, EX_MEM_Next;
+        cvsim::MEM_WB_Reg MEM_WB_Curr, MEM_WB_Next;
 
         cvsim::IF_ID_Reg fetch(cvsim::addr_t address) {
-            cvsim::word_t inst = bus.read(address);
-            cvsim::IF_ID_Reg if_id = {};
-            if_id.pc = address;
-            if_id.instruction = inst;
-            if_id.valid = true;
+            cvsim::word_t instruction = bus.read(address);
+            cvsim::IF_ID_Reg if_id;
+            if_id.instruction = instruction;
+            if_id.pc = pc;
+            if_id.valid = (instruction != 0x00000000);
             return if_id;
         }
 
         cvsim::ID_EX_Reg decode(cvsim::IF_ID_Reg if_id) {
-            // TODO: Implementar a decodificação da instrução
+
         }
 
+        cvsim::EX_MEM_Reg execute(cvsim::ID_EX_Reg id_ex) {
+
+        }
 
     public:
         CPU(Bus& system_bus) : bus(system_bus), pc(cvsim::RESET_VECTOR) {}
@@ -57,8 +57,19 @@ class CPU {
             MEM_WB_Next = {};
         }
 
-        void step() {
-            // TODO: Implementar o ciclo de busca, decodificação, execução, memória e escrita de volta
+        void eval() {
+            // Combinational logic
+            IF_ID_Next = fetch(pc);
+            ID_EX_Next = decode(IF_ID_Curr);
+            EX_MEM_Next = execute(ID_EX_Curr);
+        }
+
+        void commit() {
+            // Sequential logic
+            IF_ID_Curr = IF_ID_Next;
+            ID_EX_Curr = ID_EX_Next;
+            EX_MEM_Curr = EX_MEM_Next;
+            MEM_WB_Curr = MEM_WB_Next;
         }
 };
 
